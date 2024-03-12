513474999901d83c24e73de3276401370fec9294
arrayyan aprilyanto <146199861+arrayyan123@users.noreply.github.com>
2024-03-12 19:18:04 +0700

    revisi banyak

--- genova-react/src/Component/Navbar/Navbar.jsx
+++ genova-react/src/Component/Navbar/Navbar.jsx
@@ -12,9 +12,9 @@ function Navbar() {
   ];
   let [open, setOpen] = useState(false);
   return (
-    <div className="shadow-md md:shadow-none w-[95%] fixed top-2 left-3 md:left-8 z-10 rounded-full">
-      <div className="md:flex items-center justify-between bg-slate-100 md:bg-opacity-0 py-4 md:px-10 px-7 rounded-full">
-        <div className="font-bold text-2xl cursor-pointer flex items-center md:text-white text-gray-600 md:font-[Poppins text-gray-800] font-custom">
+    <div className="shadow-md lg:md:shadow-none w-[95%] fixed top-2 left-3 md:left-8 z-10 rounded-full">
+      <div className="lg:md:flex items-center justify-between bg-slate-100 lg:md:bg-opacity-0 py-4 lg:md:px-10 px-7 rounded-full">
+        <div className="font-bold text-2xl cursor-pointer flex items-center lg:md:text-white text-gray-600 lg:md:font-[Poppins text-gray-800] font-custom">
           <span className="w-[40px] mr-5 pt-auto">
             <img src={Logo} alt="ufest logo" />
           </span>
@@ -22,23 +22,23 @@ function Navbar() {
         </div>
         <div
           onClick={() => setOpen(!open)}
-          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+          className="text-3xl absolute right-8 top-6 cursor-pointer lg:md:hidden"
         >
           <IonIcon name={open ? "close" : "menu"}></IonIcon>
         </div>
         <ul
-          className={`rounded-3xl md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-slate-100 md:bg-opacity-0 md:z-auto z-[-1] left-0 w-full md:w-auto md:px-0 px-[200px] transition-all duration-500 ease-in ${
+          className={`rounded-3xl lg:md:flex lg:md:items-center lg:md:pb-0 pb-12 absolute lg:md:static bg-slate-100 lg:md:bg-opacity-0 md:z-auto z-[-1] left-0 w-full lg:md:w-auto lg:md:px-0 px-[200px] transition-all duration-500 ease-in ${
             open ? "top-[90px] opacity-100" : "top-[-485px]"
           } md:opacity-100 opacity-0`}
         >
           {Links.map((link) => (
             <li
               key={link.name}
-              className="md:ml-8 text-xl md:my-0 my-7 text-center "
+              className="lg:md:ml-8 text-xl lg:md:my-0 my-7 text-center "
             >
               <a
                 href={link.link}
-                className="md:text-white text-gray-800 hover:text-gray-600 md:hover:text-gray-400 duration-500 font-custom font-extrabold md:text-[20px] text-[17px]"
+                className="lg:md:text-white text-gray-800 hover:text-gray-600 lg:md:hover:text-gray-400 duration-500 font-custom font-extrabold md:text-[20px] text-[17px]"
               >
                 {link.name}
               </a>
