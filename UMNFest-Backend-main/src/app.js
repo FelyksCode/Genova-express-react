168497301d97175804f53d532fe5b22d097b2279
Felyks <gamingfelix78@gmail.com>
2024-03-30 19:04:27 +0700

    regist form prototype
    Penambahan CORS pada server side, prototype register pada client side

--- UMNFest-Backend-main/src/app.js
+++ UMNFest-Backend-main/src/app.js
@@ -1,70 +1,94 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 const RaceDetail = require("./models/RaceDetail");
-const fs = require('fs');
+const fs = require("fs");
+
+// Note: Nambah CORS
+const cors = require("cors"); // Require CORS
 
 const MONGODB_URI = process.env.MONGODB_URI;
-if(!MONGODB_URI) {
-    throw new Error("MONGODB_URI is required");
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is required");
 }
 
 const port = process.env.PORT || 3000; // default
 
+// Penambahan, ntar origin nyesuain client-nya dari mana
+app.use(cors({ origin: "http://localhost:3000" })); // Enable CORS for all routes
+
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 connectDB(MONGODB_URI).then(() => {
-    firstStart();
-})
-
-app.get('/', (req, res) => {
-    res.json({ error: false, "message": "UMNFest API", "docs": "https://elements.getpostman.com/redirect?entityId=33191415-b1a4909c-0b6d-4c0b-a65c-ea0e640f9935&entityType=collection" });
+  firstStart();
 });
 
-const apiRoutes = require('./api/');
-app.use('/', apiRoutes);
+app.get("/", (req, res) => {
+  res.json({
+    error: false,
+    message: "UMNFest API",
+    docs: "https://elements.getpostman.com/redirect?entityId=33191415-b1a4909c-0b6d-4c0b-a65c-ea0e640f9935&entityType=collection",
+  });
+});
+
+const apiRoutes = require("./api/");
+app.use("/", apiRoutes);
 
 app.listen(port, () => {
-    console.log(`UMNFest API Listening at http://localhost:${port}`);
+  console.log(`UMNFest API Listening at http://localhost:${port}`);
 });
 
-
 function firstStart() {
-    if(!fs.existsSync("public")) {
-        fs.mkdirSync("public");
+  if (!fs.existsSync("public")) {
+    fs.mkdirSync("public");
+  }
+  if (!fs.existsSync("public/uploads")) {
+    fs.mkdirSync("public/uploads");
+  }
+  var publicFolders = ["ktm", "gallery", "news"];
+  publicFolders.forEach((name) => {
+    if (!fs.existsSync(`public/uploads/${name}`)) {
+      fs.mkdirSync(`public/uploads/${name}`);
     }
-    if(!fs.existsSync("public/uploads")) {
-        fs.mkdirSync("public/uploads");
-    }
-    var publicFolders = ['ktm', 'gallery', 'news'];
-    publicFolders.forEach((name) => {
-        if(!fs.existsSync(`public/uploads/${name}`)) {
-            fs.mkdirSync(`public/uploads/${name}`);
-        }
-    })
-    RaceDetail.estimatedDocumentCount()
-        .then(count => {
-            const raceNames = ["Unveiling", "E-Ulympic", "Ulympic", "U-Care", "Unify"];
-            const raceDescription = ["Lomba Unveiling adalah...", "Lomba E-ulympic adalah...", "Lomba Ulympic adalah...", "Lomba U-Care adalah...", "Lomba Unify adalah"];
-            if (count === 0 || count != raceNames.length) {
-                var i = 0;
-                raceNames.forEach(raceName => {
-                    new RaceDetail({ race_name: raceName, race_description: raceDescription[i] }).save()
-                        .then(() => {
-                            console.log(`Added '${raceName}' into Race Details`);
-                        })
-                        .catch(err => {
-                            console.log("Error adding race detail:", err);
-                        });
-                    i++
-
-                });
-            }
-        })
-        .catch(err => {
-            console.log("Error checking race details count:", err);
+  });
+  RaceDetail.estimatedDocumentCount()
+    .then((count) => {
+      const raceNames = [
+        "Unveiling",
+        "E-Ulympic",
+        "Ulympic",
+        "U-Care",
+        "Unify",
+      ];
+      const raceDescription = [
+        "Lomba Unveiling adalah...",
+        "Lomba E-ulympic adalah...",
+        "Lomba Ulympic adalah...",
+        "Lomba U-Care adalah...",
+        "Lomba Unify adalah",
+      ];
+      if (count === 0 || count != raceNames.length) {
+        var i = 0;
+        raceNames.forEach((raceName) => {
+          new RaceDetail({
+            race_name: raceName,
+            race_description: raceDescription[i],
+          })
+            .save()
+            .then(() => {
+              console.log(`Added '${raceName}' into Race Details`);
+            })
+            .catch((err) => {
+              console.log("Error adding race detail:", err);
+            });
+          i++;
         });
+      }
+    })
+    .catch((err) => {
+      console.log("Error checking race details count:", err);
+    });
 }
--- genova-react/src/Component/Regis/RegisForm.jsx
+++ genova-react/src/Component/Regis/RegisForm.jsx
@@ -1,30 +1,91 @@
 import React, { useState } from "react";
 
 function Regisform() {
+  const [name, setName] = useState("");
+  const [nim, setNim] = useState("");
+  const [email, setEmail] = useState("");
+  const [ktm, setKtm] = useState("");
+  const [lomba, setLomba] = useState("");
+
+  function handleForm(event) {
+    event.preventDefault();
+
+    // Create an instance of FormData
+    const formData = new FormData();
+
+    // Append the text fields to formData
+    formData.append("name", name);
+    formData.append("nim", nim);
+    formData.append("email", email);
+    // formData.append("lomba", lomba); // Belum bisa gara gara schema.js bagian userRegisterSchema dari server
+
+    // Append the file to formData.
+    // The 'ktm' state should hold the file itself, not just its value.
+    // Thus, you need to adjust the onChange handler for the file input.
+    if (ktm) {
+      formData.append("ktm", ktm);
+    }
+
+    // Note: Link Fetch-nya ntar diganti nyesuain base URL api
+    fetch("http://localhost:8090/users/register/65eaf6ff92b679723f5d1d45", {
+      method: "post",
+      body: formData, // No need to set 'Content-Type' header, browser will set 'multipart/form-data' automatically
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+    console.log("Form Submitted");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleForm}>
       <div>
         <label>Nama:</label>
-        <input type="text" name="name" required />
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
       </div>
       <div>
         <label>NIM:</label>
-        <input type="number" name="nim" required />
+        <input
+          type="number"
+          name="nim"
+          onChange={(e) => setNim(e.target.value)}
+          required
+        />
       </div>
       <div>
         <label>Email:</label>
-        <input type="email" name="email" required />
+        <input
+          type="email"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
       </div>
       <div>
         <label>Photo:</label>
-        <input type="file" name="photo" accept="image/*" required />
+        <input
+          type="file"
+          name="photo"
+          accept="image/png, image/jpg, image/jpeg"
+          onChange={(e) => setKtm(e.target.files[0])}
+          required
+        />
       </div>
       <div>
         <label>Lomba:</label>
-        <select name="competition" required>
+        <select
+          name="competition"
+          onChange={(e) => setLomba(e.target.value)}
+          required
+        >
           <option value="">Pilih Lomba</option>
-          <option value="Lomba A">Mobile Legend</option>
-          <option value="Lomba B">Valorant</option>
+          <option value="Mobile Legend">Mobile Legend</option>
+          <option value="Valorant">Valorant</option>
         </select>
       </div>
       <button type="submit">Submit</button>
