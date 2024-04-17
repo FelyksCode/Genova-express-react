8261aac0cbf3d691d6619439766d13b9123e325d
Hosea <hoseahose63@gmail.com>
2024-04-17 12:02:47 +0700

    Update Footer (Sementara)
    Nanti Gua rapiin Lagi, jangan diapa-apain dulu

--- genova-react/src/Component/Footer/Footer.jsx
+++ genova-react/src/Component/Footer/Footer.jsx
@@ -6,19 +6,25 @@ export default function Footer() {
   return (
     <footer
       id="footer"
-      className="flex footer px-10 bg-neutral text-neutral-content justify-between pt-10"
+      className="flex footer px-10 bg-neutral text-neutral-content justify-between pt-5"
     >
-      <div className="flex w-full md:flex-row flex-col justify-evenly py-5 gap-20">
-        <aside className="flex flex-col justify-center self-center">
-          <Sparkle>
-            <img className="h-48 footer-img " src={Logo} alt="logo" />
-          </Sparkle>
-        </aside>
-
-        <nav className="justify-self-end">
-          <Sparkle>
-            <div className="grid grid-flow-col gap-4 ">
-              <a>
+      <div className="flex w-full md:flex-row flex-col justify-evenly items-center py-5 gap-10">
+        <div className="flex justify-around self-center">
+          <div>
+            <Sparkle>
+              <img className="h-48 footer-img " src={Logo} alt="logo" />
+            </Sparkle>
+          </div>
+          <div className="lg:hidden md:hidden">
+            <Sparkle>
+              <img className="h-48 footer-img " src={Logo} alt="logo" />
+            </Sparkle>
+          </div>
+        </div>
+        <Sparkle>
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex justify-center">
+              <a className="mx-5">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -29,7 +35,7 @@ export default function Footer() {
                   <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
                 </svg>
               </a>
-              <a>
+              <a className="mx-5">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -40,7 +46,7 @@ export default function Footer() {
                   <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
                 </svg>
               </a>
-              <a>
+              <a className="mx-5">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -52,13 +58,24 @@ export default function Footer() {
                 </svg>
               </a>
             </div>
-          </Sparkle>
-        </nav>
-        <div className="flex flex-col flex-start items-start justify-start">
+            <div className="md:flex flex-col items-center justify-center hidden">
+              <p style={{ color: "White" }} className="text-center">
+                Universitas Multimedia Nusantara
+                <br />
+                Jl. Scientia Boulevard, Gading Serpong
+                <br />
+                Tangerang, Banten
+                <br />
+                15811, Indonesia
+              </p>
+            </div>
+          </div>
+        </Sparkle>
+        <div className="flex flex-col items-center justify-center md:hidden">
           <Sparkle>
             <strong className="footer-title">ADDRESS</strong>
           </Sparkle>
-          <p style={{ color: "White" }}>
+          <p style={{ color: "White" }} className="text-center">
             Universitas Multimedia Nusantara
             <br />
             Jl. Scientia Boulevard, Gading Serpong
@@ -68,7 +85,14 @@ export default function Footer() {
             15811, Indonesia
           </p>
         </div>
+
+        <div className="md:flex flex-col justify-center self-center hidden">
+          <Sparkle>
+            <img className="h-48 footer-img " src={Logo} alt="logo" />
+          </Sparkle>
+        </div>
       </div>
+
       <div className="Footer-Foot flex justify-center self-center">
         <p className="text-center footer-text tracking-wide">
           © UMN FESTIVAL 2024 By{" "}
