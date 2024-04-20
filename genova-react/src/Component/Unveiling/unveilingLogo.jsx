c5f092927029a70ccef7be2aec206b5afa41b409
VincentSVSutiono <vincentsvsutiono@gmail.com>
2024-04-20 23:53:38 +0700

    Revisi

--- genova-react/src/Component/Unveiling/unveilingLogo.jsx
+++ genova-react/src/Component/Unveiling/unveilingLogo.jsx
@@ -4,11 +4,11 @@ import "./unveilingLogo.css";
 
 function Logo() {
   return (
-    <div className="bg-gradient-to-b from-yellow-400 to-orange-400">
+    <div className="outer">
       <div className="content flex flex-col items-center py-8 lg:flex-row lg:justify-evenly xl:items-start lg:py-16">
-        <div className="unveiling-logo mb-8 px-4 bg-white rounded-2xl lg:mb-0 lg:py-5">
+        <div className="unveiling-logo mt-12 mb-8 px-4 bg-white py-5 rounded-full lg:mb-0">
           <img
-            className="w-56 sm:w-72 lg:w-80 xl:w-96"
+            className="w-48 sm:w-64 lg:w-72 xl:w-80"
             src={logo}
             alt="Unveiling"
           />
@@ -17,22 +17,28 @@ function Logo() {
           <h1 className="text-center font-bold text-4xl sm:text-5xl sm:py-5 lg:text-5xl">
             Unveiling
           </h1>
-          <p className="bg-white rounded-2xl w-[250px] text-sm sm:w-[500px] xl:w-[800px] xl:text-lg mt-5 px-8 pt-5 pb-5 lg:mb-10">
+          <p className="rounded-2xl w-[250px] text-sm sm:w-[500px] xl:w-[700px] xl:text-lg mt-2 px-8 pt-5 pb-3">
             𝐔𝐍𝐕𝐄𝐈𝐋𝐈𝐍𝐆 merupakan rangkaian awal yang membuka UMN Festival 2024!
             Didesain untuk memperkenalkan seluruh agenda festival kepada
-            masyarakat Universitas Multimedia Nusantara! UNVEILING menciptakan
-            keinginan para #Legions untuk mengenal lebih dalam tentang UMN
-            Festival 2024 (Curiosity) dan juga meningkatkan semangat para
-            #Legions untuk menyambut UMN Festival 2024 (Enthusiastic)! Join us,
-            to kickstart the excitement, and witness what UMN Festival 2024 has
-            prepared for you!
+            masyarakat Universitas Multimedia Nusantara!
+          </p>
+          <p className="rounded-2xl w-[250px] text-sm sm:w-[500px] xl:w-[700px] xl:text-lg mt-2 px-8 pt-5 pb-3">
+            𝐔𝐍𝐕𝐄𝐈𝐋𝐈𝐍𝐆 menciptakan keinginan para #Legions untuk mengenal lebih
+            dalam tentang UMN Festival 2024 (Curiosity) dan juga meningkatkan
+            semangat para #Legions untuk menyambut UMN Festival 2024
+            (Enthusiastic)!
+          </p>
+          <p className="rounded-2xl w-[250px] text-sm sm:w-[500px] xl:w-[700px] xl:text-lg mt-2 px-8 pt-5 pb-3">
+            Join us, to kickstart the excitement, and witness what UMN Festival
+            2024 has prepared for you!
           </p>
         </div>
       </div>
+      <hr className="w-4/5 mx-auto mb-10 border-gray-800" />
       <div className="flex justify-center flex-col w-full">
-        <button className="tombol mb-10 w-2/4 self-center">
+        <button className="tombol mb-10 self-center">
           <a href="https://forms.gle/6R9D1JdEUbACJVj69" target="_blank">
-            Regular Form
+            Regular
           </a>
         </button>
       </div>
