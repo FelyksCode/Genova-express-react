8fd0e045823957498f6f188de9bc640417ee5d57
Felyks <gamingfelix78@gmail.com>
2024-04-26 16:15:35 +0700

    update unveil remove form button

--- genova-react/src/Page/Unveiling.jsx
+++ genova-react/src/Page/Unveiling.jsx
@@ -15,7 +15,7 @@ function Unveiling() {
             <ImageUnveiling />
           </Fade>
         </section>
-        <section className="snap-y snap-mandatory overflow-hidden isiucare py-20">
+        <section className="snap-y snap-mandatory overflow-hidden isiucare py-5">
           <Fade>
             <Unveilingsementara />
           </Fade>
