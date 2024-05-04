ea6a230165c577bfd25d663061f94b0268d1b459
EdwinLolo <edwinfedora@gmail.com>
2024-05-04 13:29:58 +0700

    update bracket ML

--- genova-react/src/Component/Eulympic/Esports/BracketValo.jsx
+++ genova-react/src/Component/Eulympic/Esports/BracketValo.jsx
@@ -2,12 +2,22 @@ import React from "react";
 
 function BracketValo() {
   return (
-    <div className="w-screen h-[700px] font-custom mb-4">
+    <div className="w-screen h-[900px] font-custom mb-4">
       <h1 className="text-center text-[35px] font-bold mb-2 text-[#3c3b60] underline">
-        Valorant
+        Mobile Legends
       </h1>
-      <iframe
+      {/* <iframe
         src="https://challonge.com/ioawebe6/module"
+        frameborder="0"
+        scrolling="auto"
+        allowtransparency="true"
+        className="mx-auto rounded-sm w-full h-[90%] lg:w-[50%] "
+      ></iframe> */}
+
+      <iframe
+        src="https://challonge.com/id/umnfestival_mobilelegends/module"
+        width="100%"
+        height="500"
         frameborder="0"
         scrolling="auto"
         allowtransparency="true"
